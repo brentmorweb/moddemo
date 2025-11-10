d4363203f8b71651523a0cf11d629dd905fc0751
brentmorweb <brent@morweb.org>
2025-11-10 16:23:46 -0700

    Update desktop.php

--- views/bpages/desktop.php
+++ views/bpages/desktop.php
@@ -1,26 +1,38 @@
 <?php
-/**
- * Purpose: Wraps the Pages prototype in the standard desktop chrome.
- * Usage: Injects the rendered index view so it matches the vanilla demo folder structure.
- */
-        $mwLoad->Window('wDialog', 'dialogs/wCombo');
+	$mwLoad->Window('wDialog', 'bpages/index');
 
-        $mwLoad
-                ->js('sample.js')
-                ->css('sample.css')
-        ; //$mwLoad
+	$mwLoad
+		->js('sample.js')
+		->css('sample.css')
+	; //$mwLoad
 
 ?>
 
+<style>
+
+</style>
+
 <div class="mwDskTools">
 
-        <h1><span>Pages</span> Prototype</h1>
+	<h1><span>B</span> Pages</h1>
 
-        <a class="Add" onclick="mwWindow('wDialog').show();">Dialog</a>
+	<a class="Add" onclick="mwWindow('wDialog').show();">New Page</a>
 </div>
 
 <div class="mwDesktop">
 
-        <?=$bpagesIndex?>
+	<?=$bpagesIndex?>
 
 </div>
+
+<script type="text/javascript">
+
+	jQuery( function () {
+
+		setTimeout( function () {
+		//	mwWindow('wDialog').show();
+		}, 500);
+
+	}); //jQuery
+
+</script>
